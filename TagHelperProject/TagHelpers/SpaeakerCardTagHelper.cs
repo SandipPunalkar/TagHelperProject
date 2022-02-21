a0e17fbaa4b4682d58a19657f5de437bd88ec792
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-02-21 09:27:12 +0530

    Build the simple date tag helper.

---
+++ TagHelperProject/TagHelpers/SpaeakerCardTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelperProject.TagHelpers
+{
+    //[HtmlTargetElement("tag-name")]
+    public class SpaeakerCardTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "strong";
+            output.Content.SetHtmlContent($"Date: { DateTime.Now}");
+        }
+    }
+}
