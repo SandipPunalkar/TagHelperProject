5454be903bf104a44ef10fc5616fb54e3c33dcab
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-02-22 10:08:49 +0530

    Build the Own Tag Helpers

---
+++ TagHelperProject/TagHelpers/CompanyTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelperProject.TagHelpers
+{
+    [HtmlTargetElement("company")]
+    public class CompanyTagHelper : TagHelper
+    {
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string preContent = @"
+                <div class='card card-body' style='padding: 20px'>
+                 <div class='card-header' style='margin-bottom:50px;'>
+                    <img src='/images/{0}.jpg' height='20' />
+                 </div>";
+
+            const string postContent = @"</div>";
+
+            output.TagName = "div";
+            output.PreContent.SetHtmlContent(string.Format(preContent,Name));
+            output.PostContent.SetHtmlContent(postContent);
+        }
+    }
+}
---
+++ TagHelperProject/TagHelpers/EmployeeTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelperProject.TagHelpers
+{
+    //[HtmlTargetElement("employee", Attributes ="name,title",ParentTag ="comapny")]
+    public class EmployeeTagHelper : TagHelper
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            string content = @"
+                <div class='card card-body' style='margin-top: 10px'>
+                   <img class='img-fluid' src='/images/{0}.jpg' style='float:left; margin-right:10px' />
+                   <p class='list-group-item'>{0}<br/><i>{1}</i></p>   
+                </div>";
+
+
+            output.Content.SetHtmlContent(string.Format(content, Name.Replace(" ", "-"), Title));
+        }
+    }
+}
